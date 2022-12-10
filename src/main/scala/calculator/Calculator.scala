5f9a67c9e8dbdf63147639d4e9e0073f4d5978f9
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-12-10 09:52:18 +0000

    [FPS][Design][week5] implement computeSolutions signal

--- src/main/scala/calculator/Calculator.scala
+++ src/main/scala/calculator/Calculator.scala
@@ -13,10 +13,21 @@ object Calculator extends CalculatorInterface:
 
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] =
-    ???
+    namedExpressions.map( (ref, sigExpr) =>
+      (ref, Signal {
+        eval(sigExpr(), namedExpressions - ref)
+      })
+    )
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]])(using Signal.Caller): Double =
-    ???
+    expr match {
+      case Literal(v) => v
+      case Ref(name) => eval(getReferenceExpr(name, references), references - name)
+      case Plus(a, b) => eval(a, references) + eval(b, references)
+      case Minus(a, b) => eval(a, references) - eval(b, references)
+      case Times(a, b) => eval(a, references) * eval(b, references)
+      case Divide(a, b) => eval(a, references) / eval(b, references)
+    }
 
   /** Get the Expr for a referenced variables.
    *  If the variable is not known, returns a literal NaN.
